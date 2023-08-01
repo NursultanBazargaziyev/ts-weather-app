060be4179cc89f83b6dd21916226f70cb6754ab4
Bazargaziyev Nursultan <nursultanbazargaziyev@gmail.com>
2023-08-01 16:36:56 +0200

    Netlify config

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
+/* eslint-disable no-useless-computed-key */
 export const UNITS = {
   ["metric"]: "°C",
   ["standard"]: "K",
   ["imperial"]: "°F",
 };
-
+/* eslint-enable no-useless-computed-key */
 export type UnitKeys = keyof typeof UNITS;
